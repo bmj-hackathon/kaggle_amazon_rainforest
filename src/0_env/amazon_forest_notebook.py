d29b75b027f470ba603e0a8ac1f5d5c436080bd3
MarcusJones <jones.0bj3@gmail.com>
2019-05-13 14:08:09 +0200

    refactor change to jupytext formatting

--- src/0_env/amazon_forest_notebook.py
+++ src/0_env/amazon_forest_notebook.py
@@ -1,34 +1,36 @@
 # -*- coding: utf-8 -*-
 # <nbformat>4</nbformat>
 
-# <markdowncell>
+# %% [markdown]
 
 # # Planet: Understanding the Amazon deforestation from Space challenge
 
-# <markdowncell>
+# %% [markdown]
 
 # Special thanks to the kernel contributors of this challenge (especially @anokas and @Kaggoo) who helped me find a starting point for this notebook.
-# 
+#
 # The whole code including the `data_helper.py` and `keras_helper.py` files are available on github [here](https://github.com/EKami/planet-amazon-deforestation) and the notebook can be found on the same github [here](https://github.com/EKami/planet-amazon-deforestation/blob/master/notebooks/amazon_forest_notebook.ipynb)
-# 
+#
 # **If you found this notebook useful some upvotes would be greatly appreciated! :) **
 
-# <markdowncell>
+# %% [markdown]
 
 # Start by adding the helper files to the python path
 
-# <codecell>
+# %%
 
 import sys
+from pathlib import Path
+PATH_UTILS_MODULE = Path('./src/utils')
+PATH_UTILS_MODULE.exists()
+sys.path.append(PATH_UTILS_MODULE)
+# sys.path.append('../tests')
 
-sys.path.append('../src')
-sys.path.append('../tests')
-
-# <markdowncell>
+# %% [markdown]
 
 # ## Import required modules
 
-# <codecell>
+# %%
 
 import os
 import gc
@@ -49,20 +51,20 @@ from kaggle_data.downloader import KaggleDataDownloader
 %matplotlib inline
 %config InlineBackend.figure_format = 'retina'
 
-# <markdowncell>
+# %% [markdown]
 
 # Print tensorflow version for reuse (the Keras module is used directly from the tensorflow framework)
 
-# <codecell>
+# %%
 
 tf.__version__
 
-# <markdowncell>
+# %% [markdown]
 
 # ## Download the competition files
 # Download the dataset files and extract them automatically with the help of [Kaggle data downloader](https://github.com/EKami/kaggle-data-downloader)
 
-# <codecell>
+# %%
 
 competition_name = "planet-understanding-the-amazon-from-space"
 
@@ -83,47 +85,47 @@ for dir_path in datasets_path:
 if not is_datasets_present:
     # Put your Kaggle user name and password in a $KAGGLE_USER and $KAGGLE_PASSWD env vars respectively
     downloader = KaggleDataDownloader(os.getenv("KAGGLE_USER"), os.getenv("KAGGLE_PASSWD"), competition_name)
-    
+
     train_output_path = downloader.download_dataset(train, destination_path)
     downloader.decompress(train_output_path, destination_path) # Outputs a tar file
     downloader.decompress(destination_path + train_u, destination_path) # Extract the content of the previous tar file
     os.remove(train_output_path) # Removes the 7z file
     os.remove(destination_path + train_u) # Removes the tar file
-    
+
     test_output_path = downloader.download_dataset(test, destination_path)
     downloader.decompress(test_output_path, destination_path) # Outputs a tar file
     downloader.decompress(destination_path + test_u, destination_path) # Extract the content of the previous tar file
     os.remove(test_output_path) # Removes the 7z file
     os.remove(destination_path + test_u) # Removes the tar file
-    
+
     test_add_output_path = downloader.download_dataset(test_additional, destination_path)
     downloader.decompress(test_add_output_path, destination_path) # Outputs a tar file
     downloader.decompress(destination_path + test_additional_u, destination_path) # Extract the content of the previous tar file
     os.remove(test_add_output_path) # Removes the 7z file
     os.remove(destination_path + test_additional_u) # Removes the tar file
-    
+
     test_labels_output_path = downloader.download_dataset(test_labels, destination_path)
     downloader.decompress(test_labels_output_path, destination_path) # Outputs a csv file
     os.remove(test_labels_output_path) # Removes the zip file
 else:
     print("All datasets are present.")
 
-# <markdowncell>
+# %% [markdown]
 
 # ## Inspect image labels
 # Visualize what the training set looks like
 
-# <codecell>
+# %%
 
 train_jpeg_dir, test_jpeg_dir, test_jpeg_additional, train_csv_file = data_helper.get_jpeg_data_files_paths()
 labels_df = pd.read_csv(train_csv_file)
 labels_df.head()
 
-# <markdowncell>
+# %% [markdown]
 
 # Each image can be tagged with multiple tags, lets list all uniques tags
 
-# <codecell>
+# %%
 
 # Print all unique tags
 from itertools import chain
@@ -131,26 +133,26 @@ labels_list = list(chain.from_iterable([tags.split(" ") for tags in labels_df['t
 labels_set = set(labels_list)
 print("There is {} unique labels including {}".format(len(labels_set), labels_set))
 
-# <markdowncell>
+# %% [markdown]
 
 # ### Repartition of each labels
 
-# <codecell>
+# %%
 
 # Histogram of label instances
 labels_s = pd.Series(labels_list).value_counts() # To sort them by count
 fig, ax = plt.subplots(figsize=(16, 8))
 sns.barplot(x=labels_s, y=labels_s.index, orient='h')
 
-# <markdowncell>
+# %% [markdown]
 
 # ## Images
 # Visualize some chip images to know what we are dealing with.
 # Lets vizualise 1 chip for the 17 images to get a sense of their differences.
 
-# <codecell>
+# %%
 
-images_title = [labels_df[labels_df['tags'].str.contains(label)].iloc[i]['image_name'] + '.jpg' 
+images_title = [labels_df[labels_df['tags'].str.contains(label)].iloc[i]['image_name'] + '.jpg'
                 for i, label in enumerate(labels_set)]
 
 plt.rc('axes', grid=False)
@@ -162,63 +164,63 @@ for i, (image_name, label) in enumerate(zip(images_title, labels_set)):
     axs[i].imshow(img)
     axs[i].set_title('{} - {}'.format(image_name, label))
 
-# <markdowncell>
+# %% [markdown]
 
 # # Image resize & validation split
-# Define the dimensions of the image data trained by the network. Recommended resized images could be 32x32, 64x64, or 128x128 to speedup the training. 
-# 
+# Define the dimensions of the image data trained by the network. Recommended resized images could be 32x32, 64x64, or 128x128 to speedup the training.
+#
 # You could also use `None` to use full sized images.
-# 
+#
 # Be careful, the higher the `validation_split_size` the more RAM you will consume.
 
-# <codecell>
+# %%
 
 img_resize = (128, 128) # The resize size of each image ex: (64, 64) or None to use the default image size
 validation_split_size = 0.2
 
-# <markdowncell>
+# %% [markdown]
 
 # # Data preprocessing
-# Due to the hudge amount of memory the preprocessed images can take, we will create a dedicated `AmazonPreprocessor` class which job is to preprocess the data right in time at specific steps (training/inference) so that our RAM don't get completely filled by the preprocessed images. 
-# 
+# Due to the hudge amount of memory the preprocessed images can take, we will create a dedicated `AmazonPreprocessor` class which job is to preprocess the data right in time at specific steps (training/inference) so that our RAM don't get completely filled by the preprocessed images.
+#
 # The only exception to this being the validation dataset as we need to use it as-is for f2 score calculation as well as when we calculate the validation accuracy of each batch.
 
-# <codecell>
+# %%
 
-preprocessor = AmazonPreprocessor(train_jpeg_dir, train_csv_file, test_jpeg_dir, test_jpeg_additional, 
+preprocessor = AmazonPreprocessor(train_jpeg_dir, train_csv_file, test_jpeg_dir, test_jpeg_additional,
                                   img_resize, validation_split_size)
 preprocessor.init()
 
-# <codecell>
+# %%
 
 print("X_train/y_train length: {}/{}".format(len(preprocessor.X_train), len(preprocessor.y_train)))
 print("X_val/y_val length: {}/{}".format(len(preprocessor.X_val), len(preprocessor.y_val)))
 print("X_test/X_test_filename length: {}/{}".format(len(preprocessor.X_test), len(preprocessor.X_test_filename)))
 preprocessor.y_map
 
-# <markdowncell>
+# %% [markdown]
 
 # # Funetuning
-# 
+#
 # Here we define the model for finetuning
 
-# <codecell>
+# %%
 
 model = vgg16.create_model(img_dim=(128, 128, 3))
 model.summary()
 
-# <markdowncell>
+# %% [markdown]
 
 # ## Fine-tune conv layers
-# We will now finetune all layers in the VGG16 model. 
+# We will now finetune all layers in the VGG16 model.
 
-# <codecell>
+# %%
 
 history = History()
-callbacks = [history, 
+callbacks = [history,
              EarlyStopping(monitor='val_loss', patience=3, verbose=1, min_delta=1e-4),
              ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=1, cooldown=0, min_lr=1e-7, verbose=1),
-             ModelCheckpoint(filepath='weights/weights.best.hdf5', verbose=1, save_best_only=True, 
+             ModelCheckpoint(filepath='weights/weights.best.hdf5', verbose=1, save_best_only=True,
                              save_weights_only=True, mode='auto')]
 
 X_train, y_train = preprocessor.X_train, preprocessor.y_train
@@ -229,14 +231,14 @@ train_generator = preprocessor.get_train_generator(batch_size)
 steps = len(X_train) / batch_size
 
 model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics = ['accuracy'])
-history = model.fit_generator(train_generator, steps, epochs=25, verbose=1, 
+history = model.fit_generator(train_generator, steps, epochs=25, verbose=1,
                     validation_data=(X_val, y_val), callbacks=callbacks)
 
-# <markdowncell>
+# %% [markdown]
 
 # ## Visualize Loss Curve
 
-# <codecell>
+# %%
 
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -246,57 +248,57 @@ plt.xlabel('epoch')
 plt.legend(['train', 'validation'], loc='upper left')
 plt.show()
 
-# <markdowncell>
+# %% [markdown]
 
 # ## Load Best Weights
 
-# <codecell>
+# %%
 
 model.load_weights("weights/weights.best.hdf5")
 print("Weights loaded")
 
-# <markdowncell>
+# %% [markdown]
 
 # ## Check Fbeta Score
 
-# <codecell>
+# %%
 
 fbeta_score = vgg16.fbeta(model, X_val, y_val)
 
 fbeta_score
 
-# <markdowncell>
+# %% [markdown]
 
 # ## Make predictions
 
-# <codecell>
+# %%
 
 predictions, x_test_filename = vgg16.predict(model, preprocessor, batch_size=128)
-print("Predictions shape: {}\nFiles name shape: {}\n1st predictions ({}) entry:\n{}".format(predictions.shape, 
+print("Predictions shape: {}\nFiles name shape: {}\n1st predictions ({}) entry:\n{}".format(predictions.shape,
                                                                               x_test_filename.shape,
                                                                               x_test_filename[0], predictions[0]))
 
-# <markdowncell>
+# %% [markdown]
 
 # Before mapping our predictions to their appropriate labels we need to figure out what threshold to take for each class
 
-# <codecell>
+# %%
 
 thresholds = [0.2] * len(labels_set)
 
-# <markdowncell>
+# %% [markdown]
 
 # Now lets map our predictions to their tags by using the thresholds
 
-# <codecell>
+# %%
 
 predicted_labels = vgg16.map_predictions(preprocessor, predictions, thresholds)
 
-# <markdowncell>
+# %% [markdown]
 
 # Finally lets assemble and visualize our predictions for the test dataset
 
-# <codecell>
+# %%
 
 tags_list = [None] * len(predicted_labels)
 for i, tags in enumerate(predicted_labels):
@@ -304,30 +306,30 @@ for i, tags in enumerate(predicted_labels):
 
 final_data = [[filename.split(".")[0], tags] for filename, tags in zip(x_test_filename, tags_list)]
 
-# <codecell>
+# %%
 
 final_df = pd.DataFrame(final_data, columns=['image_name', 'tags'])
 print("Predictions rows:", final_df.size)
 final_df.head()
 
-# <codecell>
+# %%
 
 tags_s = pd.Series(list(chain.from_iterable(predicted_labels))).value_counts()
 fig, ax = plt.subplots(figsize=(16, 8))
 sns.barplot(x=tags_s, y=tags_s.index, orient='h');
 
-# <markdowncell>
+# %% [markdown]
 
 # If there is a lot of `primary` and `clear` tags, this final dataset may be legit...
 
-# <markdowncell>
+# %% [markdown]
 
 # And save it to a submission file
 
-# <codecell>
+# %%
 
 final_df.to_csv('../submission_file.csv', index=False)
 
-# <markdowncell>
+# %% [markdown]
 
 # #### That's it, we're done!
